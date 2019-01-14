e76130064ac34be886ff3fb26477589d8419d81e
Glauber Gomes Barcelos <glauberbarcelos@hotmail.com>
2019-01-13 22:39:56 -0200

    First Commit

---
+++ src/SSHRestart/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SSHRestart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textIP = new System.Windows.Forms.TextBox();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(402, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SSH Server Restart Network";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP Address";
+            // 
+            // textIP
+            // 
+            this.textIP.Location = new System.Drawing.Point(133, 110);
+            this.textIP.Name = "textIP";
+            this.textIP.Size = new System.Drawing.Size(306, 20);
+            this.textIP.TabIndex = 2;
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(133, 148);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(306, 20);
+            this.textUser.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Username";
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(133, 184);
+            this.textPass.Name = "textPass";
+            this.textPass.Size = new System.Drawing.Size(306, 20);
+            this.textPass.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(43, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(396, 58);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "RESTART";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 302);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SSH Restart";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textIP;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textPass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ src/SSHRestart/Form1.cs
@@ -0,0 +1,35 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSHRestart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Ip = textIP.Text;
+            var User = textUser.Text;
+            var Password = textPass.Text;
+            using (var client = new SshClient(Ip, User, Password))
+            {
+                client.Connect();
+                //Aqui tu coloca o comando que tu quer executar
+                client.RunCommand("etc/init.d/networking restart");
+                client.Disconnect();
+            }
+        }
+    }
+}
